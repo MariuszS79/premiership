a92f3ac73a96a8765416098bfaad108f15ec0733
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2021-06-07 14:46:29 +0100

    1

--- premiership.py
+++ premiership.py
@@ -1,2 +1,10 @@
-header=["Club", "MP", "W", "D", "L", "GF", "GA", "GD", "Pts"]
-Arsenal=["Arsenal", 0, 0, 0, 0, 0, 0, 0, 0]
+from tabulate import tabulate
+
+headers=["Club", "MP", "W", "D", "L", "GF", "GA", "GD", "Pts"]
+arsenal=["Arsenal", 0, 0, 0, 0, 0, 0, 0, 0]
+aston_villa=["Aston Villa", 0, 0, 0, 0, 0, 0, 0, 0]
+brentford=["Brentford", 0, 0, 0, 0, 0, 0, 0, 0]
+brighton=["Brighton", 0, 0, 0, 0, 0, 0, 0, 0]
+burnley=["Burnley", 0, 0, 0, 0, 0, 0, 0, 0]
+
+print(tabulate([arsenal, aston_villa, brentford, brighton, burnley], headers=["Club", "MP", "W", "D", "L", "GF", "GA", "GD", "Pts"], tablefmt='orgtbl'))
