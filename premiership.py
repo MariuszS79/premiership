0656b30446e45a09021ecb45cad1a704534422bb
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2021-06-15 00:06:47 +0100

    Update premiership.py

--- premiership.py
+++ premiership.py
@@ -1,7 +1,6 @@
 from tabulate import tabulate
 import random
 
-
 headers=["Club", "MP", "W", "D", "L", "GF", "GA", "GD", "Pts"]
 arsenal=["Arsenal", 0, 0, 0, 0, 0, 0, 0, 0]
 aston_villa=["Aston Villa", 0, 0, 0, 0, 0, 0, 0, 0]
